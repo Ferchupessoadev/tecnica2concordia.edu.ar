60ae06301f7758bd147cf9b5939afd6acfb33540
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2025-06-24 13:56:17 -0300

    add h1 in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,24 @@
 <x-layouts.app-header title="E.E.T. N° 2 Independencia">
     <main class="flex flex-col items-between">
-        <x-carousel class="h-[calc(100vh-62px)]" dark>
-            <x-carousel.item image="{{ asset('slider-banner/1.jpg') }}" alt="formation"></x-carousel.item>
-            <x-carousel.item image="{{ asset('slider-banner/2.jpg') }}" alt="formation"></x-carousel.item>
-            <x-carousel.item image="{{ asset('slider-banner/3.jpg') }}" alt="formation"></x-carousel.item>
-        </x-carousel>
-
+        <div class="h-[calc(100vh-70px)]">
+            <x-carousel class="h-full z-10" dark>
+                <x-carousel.item image="{{ asset('slider-banner/1.jpg') }}" alt=""></x-carousel.item>
+                <x-carousel.item image="{{ asset('slider-banner/2.jpg') }}" alt=""></x-carousel.item>
+                <x-carousel.item image="{{ asset('slider-banner/3.jpg') }}" alt=""></x-carousel.item>
+            </x-carousel>
+            <div class="absolute inset-0 flex flex-col justify-center items-start px-6 md:px-8">
+                <h1 class="text-2xl md:text-5xl font-bold text-center text-white z-10">E.E.T. N° 2 Independencia</h1>
+                <p class="text-lg md:text-2xl font-bold text-center text-white z-10">Formando mediante talleres prácticos y una fuerte vinculación con el mundo laboral.</p>
+                <div class="flex gap-3 items-center z-10 mt-4">
+                    <a target="_blank" href="https://www.instagram.com/escuelatecnican2/">
+                        <img src="{{ asset('instagram.ico') }}" class="transition-normal duration-300 size-7 md:size-10 hover:scale-110">
+                    </a>
+                    <a target="_blank" href="https://www.facebook.com/profile.php?id=100064268132995">
+                        <img src="{{ asset('facebook.ico') }}" class="transition-normal duration-300 size-7 md:size-10 hover:scale-110">
+                    </a>
+                </div>
+            </div>
+        </div>
 
         <flux:container>
             <!-- news -->
