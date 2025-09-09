7ae78daecddb825f3502c53ff66f441e5964cb81
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2025-09-08 23:56:56 -0300

    add: welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,25 @@
 <x-layouts.app-header title="E.E.T. N° 2 Independencia">
-    <main class="flex flex-col items-between">
-        <div class="h-[calc(100vh-70px)]">
-            <x-carousel class="h-72 z-10" dark>
-                <x-carousel.item image="{{ asset('slider-banner/1.jpg') }}" alt=""></x-carousel.item>
-                <x-carousel.item image="{{ asset('slider-banner/2.jpg') }}" alt=""></x-carousel.item>
-                <x-carousel.item image="{{ asset('slider-banner/3.jpg') }}" alt=""></x-carousel.item>
-            </x-carousel>
+    <main class="flex flex-col items-between gap-10">
+        <div class="h-[calc(100vh-70px)] flex gap-4 flex-col md:flex-row py-8">
+            <div class="flex flex-col justify-center items-start px-6 md:px-8 w-full md:w-[60%] gap-5">
+                <div class="flex flex-col">
+                    <h1 class="text-3xl font-extrabold text-dark dark:text-white">E.E.T N° 2 Independencia</h1>
+                    <p class="text-lg text-gray-900 dark:text-gray-300">La Escuela Técnica N° 2 "Independencia" de Concordia forma a sus estudiantes mediante talleres prácticos y una fuerte vinculación con el mundo laboral, preparándolos para enfrentar desafíos profesionales y académicos.</p>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <a href="https://maps.app.goo.gl/hqNWKqgmKZR8wqFY9" target="_blank" class="text-white text-sm z-10 flex items-center gap-2">
+                        <flux:icon.map-pin class="size-5" />
+                        <span>Alberdi 223, Concordia, Entre Ríos</span>
+                    </a>
+                    <a href="https://www.instagram.com/tecnica2independencia/" target="_blank" class="text-white text-sm z-10 flex items-center gap-2">
+                        <flux:icon.instagram class="size-5" />
+                        <span>tecnica2independencia</span>
+                    </a>
+                </div>
+            </div>
 
-            <div class="flex flex-col justify-center items-start px-6 md:px-8">
-                <h1 class="text-xl md:text-4xl font-bold text-white z-20">E.E.T N° 2 Independencia</h1>
-                <p class="text-sm md:text-lg font-bold text-white z-20">Formando mediante talleres prácticos y una fuerte vinculación con el mundo laboral</p>
+            <div class="w-full md:w-[40%] flex justify-center items-center px-6">
+                <img class="border-white border-2 rounded-xl w-full h-72 object-cover md:h-max" src="{{ asset('banner.jpg') }}" alt="">
             </div>
         </div>
 
