05aef23360f689a44c2796ff549bbb9c6114a257
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2025-09-08 23:57:32 -0300

    add: component footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="bg-[var(--color-surface)] text-[var(--color-surface-content)] dark:bg-[var(--color-surface)] dark:text-[var(--color-surface-content)]">
+    <div class="bg-[var(--color-surface)] text-[var(--color-surface-content)] dark:bg-[var(--color-surface)] dark:text-[var(--color-surface-content)] py-6">
+        <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+            <p class="text-center text-sm text-gray-500 dark:text-gray-400">
+                &copy; {{ date('Y') }} Técnica 2 Concordia. Todos los derechos reservados.
+            </p>
+        </div>
+    </div>
+</footer>
